0f9971ea4213e7b80dbb67e7254c94216ee92f0f
Dean Overton <64393357+Dean-Overton@users.noreply.github.com>
2023-10-23 17:53:01 +1100

    README and server availability class

--- 3-125-computer-server-availability.py
+++ 3-125-computer-server-availability.py
@@ -3,64 +3,78 @@ import subprocess
 import platform
 
 
-# List of Oracle servers located at data-pc.. systems in 3.125.
-computers = ["data-pc01.adeis.uow.edu.au",
-             "data-pc02.adeis.uow.edu.au",
-             "data-pc03.adeis.uow.edu.au",
-             "data-pc04.adeis.uow.edu.au",
-             "data-pc05.adeis.uow.edu.au",
-             "data-pc06.adeis.uow.edu.au",
-             "data-pc07.adeis.uow.edu.au",
-             "data-pc08.adeis.uow.edu.au",
-             "data-pc09.adeis.uow.edu.au",
-             "data-pc10.adeis.uow.edu.au",
-             "data-pc11.adeis.uow.edu.au",
-             "data-pc12.adeis.uow.edu.au",
-             "data-pc13.adeis.uow.edu.au",
-             "data-pc14.adeis.uow.edu.au",
-             "data-pc15.adeis.uow.edu.au",
-             "data-pc16.adeis.uow.edu.au",
-             "data-pc17.adeis.uow.edu.au",
-             "data-pc18.adeis.uow.edu.au",
-             "data-pc19.adeis.uow.edu.au",
-             "data-pc20.adeis.uow.edu.au",
-             "data-pc21.adeis.uow.edu.au",
-             "data-pc22.adeis.uow.edu.au",
-             "data-pc23.adeis.uow.edu.au",
-             "data-pc24.adeis.uow.edu.au",
-             "data-pc25.adeis.uow.edu.au",
-             "data-pc26.adeis.uow.edu.au",
-             "data-pc27.adeis.uow.edu.au",
-             "data-pc28.adeis.uow.edu.au",
-             "data-pc29.adeis.uow.edu.au"]
+availabilty = {}
 
 
-def ping_computer(computer):
-    """Ping a computer and print if it is online or offline"""
+class OracleServerAvailability:
+    # List of Oracle servers located at data-pc.. systems in 3.125.
+    computers = ["data-pc01.adeis.uow.edu.au",
+                 "data-pc02.adeis.uow.edu.au",
+                 "data-pc03.adeis.uow.edu.au",
+                 "data-pc04.adeis.uow.edu.au",
+                 "data-pc05.adeis.uow.edu.au",
+                 "data-pc06.adeis.uow.edu.au",
+                 "data-pc07.adeis.uow.edu.au",
+                 "data-pc08.adeis.uow.edu.au",
+                 "data-pc09.adeis.uow.edu.au",
+                 "data-pc10.adeis.uow.edu.au",
+                 "data-pc11.adeis.uow.edu.au",
+                 "data-pc12.adeis.uow.edu.au",
+                 "data-pc13.adeis.uow.edu.au",
+                 "data-pc14.adeis.uow.edu.au",
+                 "data-pc15.adeis.uow.edu.au",
+                 "data-pc16.adeis.uow.edu.au",
+                 "data-pc17.adeis.uow.edu.au",
+                 "data-pc18.adeis.uow.edu.au",
+                 "data-pc19.adeis.uow.edu.au",
+                 "data-pc20.adeis.uow.edu.au",
+                 "data-pc21.adeis.uow.edu.au",
+                 "data-pc22.adeis.uow.edu.au",
+                 "data-pc23.adeis.uow.edu.au",
+                 "data-pc24.adeis.uow.edu.au",
+                 "data-pc25.adeis.uow.edu.au",
+                 "data-pc26.adeis.uow.edu.au",
+                 "data-pc27.adeis.uow.edu.au",
+                 "data-pc28.adeis.uow.edu.au",
+                 "data-pc29.adeis.uow.edu.au"]
 
-    if platform.system() == "Windows":
-        # Windows ping command
-        ping = ["ping", "-n", "2", computer]
-    elif platform.system() == "Linux":
-        # Linux ping command
-        ping = ["ping", "-c", "2", computer]
-    else:
-        raise Exception("Unknown operating system")
+    def _ping_computer(self, computer):
+        """Ping a computer and print if it is online or offline"""
+        if platform.system() == "Windows":
+            # Windows ping command
+            ping = ["ping", "-n", "2", computer]
+        elif platform.system() == "Linux":
+            # Linux ping command
+            ping = ["ping", "-c", "2", computer]
+        else:
+            raise Exception("Unknown operating system")
 
-    if subprocess.call(ping,
-                       stdout=subprocess.DEVNULL,
-                       stderr=subprocess.DEVNULL) == 0:
-        print(f"{computer} is online")
-    else:
-        print(f"{computer} is offline")
+        if subprocess.call(ping,
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL) == 0:
+            availabilty[computer] = True
+            print(f"{computer} is online")
+        else:
+            availabilty[computer] = False
+            print(f"{computer} is offline")
+
+    def _check_vpn(self):
+        # TODO: Check if VPN is connected to the UOW network
+        print("Checking VPN connection...")
+
+    def get_availability(self):
+        """Get the availability of all computers"""
+        # Create a thread for each computer ping and start it
+        threads = []
+        for computer in self.computers:
+            thread = threading.Thread(target=self._ping_computer,
+                                      args=(computer,))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
 
 
-# Create a thread for each computer ping and start it
-threads = []
-for computer in computers:
-    thread = threading.Thread(target=ping_computer, args=(computer,))
-    threads.append(thread)
-    thread.start()
-
-for thread in threads:
-    thread.join()
+if __name__ == "__main__":
+    OracleServerAvailability().get_availability()
---
+++ README.md
@@ -0,0 +1,12 @@
+# CSIT Tools
+
+This repository contains a collection of tools developed to help CSIT students of UOW.
+
+## 3.125 Computer Server Availability Checker
+
+This tool checks the availability of the room 3.125 computer servers for Oracle. To use it, simply run the script and enter the IP address of the server. The script will then ping the server and display whether it is available or not.
+
+### Usage
+1) Install [latest](https://www.python.org/downloads/) python.
+2) If working remotely: Ensure to establish your connection through VPN first otherwise all servers will appear offline.
+3) Run it: ```python 3-125-computer-server-availability.py```
